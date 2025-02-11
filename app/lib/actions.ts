b66e1ae7b1e06c6acaf541c8e612a88f0f7cab20
Ryan Blaquiere <rblaquiere@protk.com>
2025-02-11 15:08:37 -0500

    more linting

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -131,9 +131,6 @@ export async function deleteInvoice(id: string) {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
     } catch (error) {
         console.log(error)
-        return {
-            message: 'Database Error: Failed to Delete Invoice.'
-        };
     }
     revalidatePath('/dashboard/invoices');
 }
